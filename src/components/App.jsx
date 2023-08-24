7fd8dcc9c23674a8f20c30666f12195c23305ab3
Ayushi Thakur <1089ayushiraj@gmail.com>
2023-08-25 01:15:04 +0530

    optimised code using arrow function inside map

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,28 +2,23 @@ import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import Note from './Note';
-import notes from '../notes.jsx'
+import notes from '../notes'
 
-function createNote(noteItem){
-    return <Note
-    key={noteItem.key}
-    title={noteItem.title}
-    content={noteItem.content}
-/>
-}
+
+
 
 function App(props) {
     return (
         <div>
             <Header />
+           {notes.map(noteItem=>
+                 <Note
+                 key={noteItem.key}
+                 title={noteItem.title}
+                 content={noteItem.content}
+                 />
+)}
             
-             {notes.map(noteItem=>
-               <Note
-                key={noteItem.key}
-                title={noteItem.title}
-                content={noteItem.content}
-              />
-             )}
 
             <Footer />
         </div>
